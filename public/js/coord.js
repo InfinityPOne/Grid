53ec99a28e5192f93479bff7f726525528bdfe51
GreenCillah <thecoolgreenman@gmail.com>
2017-07-10 23:19:52 +1000

    WIP delete array

--- public/js/coord.js
+++ public/js/coord.js
@@ -1,10 +1,15 @@
-//  -----> coordinators file
+//  -----> coordinators file <-----
+//  ----------------------------------------------------------------- 
+// keep this simple for the time being until I get my head around the maths. 
+// once done i'll move it to a class
+//
+//  ----------------------------------------------------------------- 
 
 var coords = [];
 
 function fn_buildInitialCoors() {
 
-    for (var i = 0; i < 1; i++) {
+    for (var i = 0; i < 10; i++) {
         for (var j = 0; j < 10; j++) {
             coords.push({
                 x: i,
@@ -16,10 +21,56 @@ function fn_buildInitialCoors() {
 
     }
 
+    /*
     coords.forEach(function (coord, index, array) {
-        console.log(coord.x + "<>" + coord.y + "<>" + coord.xG + "<>" + coord.yG)
+        console.log(coord.x + "<>" + coord.y + "<>" + coord.xG + "<>" + coord.yG);
+
+    }); */
+
+    fn_MoveRight();
+
+    return;
+}
+
+
+function fn_MoveRight() {
+
+    var farLeft = 10000;
+    var farRight = 0;
+
+    coords.forEach(function (coord, index, array) {
+
+        if (coord.x < farLeft) {
+            farLeft = coord.x;
+        }
+
+        if (coord.x > farRight) {
+            farRight = coord.x;
+        }
 
     });
 
-    return;
+    // delete left Column
+    coords.forEach(function (coord, index, array) {
+
+        console.log("xxxx" + coord.x);
+        console.log(coord.x == farLeft);
+
+        ////HHHHHHHHHHHHHERERRERERERERER
+
+        if (coord.x == farLeft) {
+            console.log(index + "|||" + coord.x);
+            coords.splice(index, 1);
+        }
+
+    });
+
+    coords.forEach(function (coord, index, array) {
+        /*
+                console.log(coord.x == farLeft);
+          */
+        console.log("moveright" + coord.x + "<>" + coord.y + "<>" + coord.xG + "<>" + coord.yG);
+
+    });
+
 }
